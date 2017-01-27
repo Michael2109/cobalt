27df5ebc08cb384a74e050dba87641e7ada9a65a
Michael <michael.jhaywoods@gmail.com>
2017-01-27 13:26:20 +0000

    Refined CharacterParser

--- src/main/scala/compiler/parser/primitives/CharacterParser.scala
+++ src/main/scala/compiler/parser/primitives/CharacterParser.scala
@@ -6,14 +6,13 @@ import compiler.parser.Parser
 import compiler.tokenizer.Tokenizer
 
 class CharacterParser extends Parser[CharacterBlock] {
-  def shouldParse(line: String): Boolean = {
-    return line.matches("char [a-zA-Z][a-zA-Z0-9]* [=] '[a-zA-Z0-9]'")
-  }
+
+  def shouldParse(line: String): Boolean = line.matches("char [a-zA-Z][a-zA-Z0-9]* [=] '[a-zA-Z0-9]'")
 
   def parse(superBlock: Block, tokenizer: Tokenizer): CharacterBlock = {
-    tokenizer.nextToken // skip char
+    tokenizer.nextToken // skip "char"
     val name: String = tokenizer.nextToken.getToken //get name
-    tokenizer.nextToken // skip =
+    tokenizer.nextToken // skip "="
     //System.out.println(tokenizer.nextToken().getToken());  // skip '
     tokenizer.nextToken
     val value: String = tokenizer.nextToken.getToken
