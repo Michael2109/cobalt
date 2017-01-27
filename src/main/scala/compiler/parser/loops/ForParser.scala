f1b228620f842464beb51ace3603aed55c7b344c
Michael <michael.jhaywoods@gmail.com>
2017-01-27 13:20:58 +0000

    Refined ForParser

--- src/main/scala/compiler/parser/loops/ForParser.scala
+++ src/main/scala/compiler/parser/loops/ForParser.scala
@@ -9,14 +9,14 @@ import compiler.tokenizer.{Token, Tokenizer}
 need to make parameter single variable names instead as cant define a variable in an ifs...
  */
 class ForParser extends Parser[ForBlock] {
-  def shouldParse(line: String): Boolean = {
-    return line.matches("for \\([a-zA-Z][a-zA-Z0-9]* [a-zA-Z][a-zA-Z0-9]* = [0-9]+([.][0-9]*)?; [a-zA-Z][a-zA-Z0-9]* [<|>|<=|>=|==] [0-9]+([.][0-9]*)?; [a-zA-Z][a-zA-Z0-9]*\\+\\+\\):")
-  }
 
+  def shouldParse(line: String): Boolean = line.matches("for \\([a-zA-Z][a-zA-Z0-9]* [a-zA-Z][a-zA-Z0-9]* = [0-9]+([.][0-9]*)?; [a-zA-Z][a-zA-Z0-9]* [<|>|<=|>=|==] [0-9]+([.][0-9]*)?; [a-zA-Z][a-zA-Z0-9]*\\+\\+\\):")
+  
   def parse(superBlock: Block, tokenizer: Tokenizer): ForBlock = {
-    tokenizer.nextToken //skip ifs
-    tokenizer.nextToken // skip (
+    tokenizer.nextToken //skip "for"
+    tokenizer.nextToken // skip "("
     val first: Token = tokenizer.nextToken
     return new ForBlock(superBlock, first.getToken)
   }
+
 }
