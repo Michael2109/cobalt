c847977121346c38a85a5740d8a83b101e08e8ad
Michael <michael.jhaywoods@gmail.com>
2017-01-27 13:23:17 +0000

    Refined DivideParser

--- src/main/scala/compiler/parser/operators/DivideParser.scala
+++ src/main/scala/compiler/parser/operators/DivideParser.scala
@@ -6,13 +6,11 @@ import compiler.parser.Parser
 import compiler.tokenizer.Tokenizer
 
 class DivideParser extends Parser[DivideBlock] {
-  def shouldParse(line: String): Boolean = {
-    return line.matches("[a-zA-Z][a-zA-Z0-9]* [/][=] [0-9]+")
-  }
+  def shouldParse(line: String): Boolean = line.matches("[a-zA-Z][a-zA-Z0-9]* [/][=] [0-9]+")
 
   def parse(superBlock: Block, tokenizer: Tokenizer): DivideBlock = {
     val name: String = tokenizer.nextToken.getToken
-    tokenizer.nextToken.getToken
+    tokenizer.nextToken
     val value: String = tokenizer.nextToken.getToken
     return new DivideBlock(superBlock, name, value)
   }
