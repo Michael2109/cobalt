734e007c7eb4dbe10955b207824c309a6c72a350
Michael <michael.jhaywoods@gmail.com>
2017-01-27 13:40:39 +0000

    Refined Token

--- src/main/scala/compiler/tokenizer/Token.scala
+++ src/main/scala/compiler/tokenizer/Token.scala
@@ -4,15 +4,10 @@ import compiler.tokenizer.TokenType.TokenType
 
 class Token(var token: String, var `type`: TokenType) {
 
-  def getToken: String = {
-    return token
-  }
+  def getToken: String = token
 
-  def getType: TokenType = {
-    return `type`
-  }
+  def getType: TokenType = `type`
 
-  override def toString: String = {
-    return token
-  }
+  override def toString: String = token
+
 }
