163d7bed550ca212946f0512480d1976193c8051
Michael <michael.jhaywoods@gmail.com>
2017-01-27 13:24:44 +0000

    Refined IntegerParser

--- src/main/scala/compiler/parser/primitives/IntegerParser.scala
+++ src/main/scala/compiler/parser/primitives/IntegerParser.scala
@@ -6,9 +6,8 @@ import compiler.parser.Parser
 import compiler.tokenizer.Tokenizer
 
 class IntegerParser extends Parser[IntegerBlock] {
-  def shouldParse(line: String): Boolean = {
-    return line.matches("int[ ]+[a-zA-Z][a-zA-Z0-9]*[ ]+[=][ ]+[0-9]+")
-  }
+
+  def shouldParse(line: String): Boolean = line.matches("int[ ]+[a-zA-Z][a-zA-Z0-9]*[ ]+[=][ ]+[0-9]+")
 
   def parse(superBlock: Block, tokenizer: Tokenizer): IntegerBlock = {
     tokenizer.nextToken // skip int
