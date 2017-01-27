168e0f0248e5895f2e56defe142939593c9ecb34
Michael <michael.jhaywoods@gmail.com>
2017-01-27 13:52:22 +0000

    Refined Parameter

--- src/main/scala/compiler/Parameter.scala
+++ src/main/scala/compiler/Parameter.scala
@@ -1,35 +1,31 @@
 package compiler
 
 // Represents what is input for ASM. E.g. int = 'I', String = 'Ljava/lang/String;'
-class Parameter(val typeInit: String, var nameInit: String) {
+class Parameter(val `type`: String, var name: String) {
 
-  private val name: String = nameInit
-  private var `type`: String = typeInit
 
-  def getName: String = {
-    return nameInit
-  }
 
-  // todo add util method to get the asm type from a variable/value
-  def getAsmType: String = {
-    if (getType == "int") {
-      return "I"
-    }
-    if (getType == "String") {
-      return "Ljava/lang/String;"
-    }
-    return null
-  }
+  // Name of the variable
+  def getName: String = name
 
-  override def toString: String = {
-    return getType + " : " + name
-  }
-
-  def getType: String = {
-    return `type`
-  }
+  // Type of the variable
+  def getType: String = `type`
 
   def setType(`type`: String) {
     this.`type` = `type`
   }
+
+  // ASM type. int = "I", String = "java.lang.String", etc
+  def getAsmType: String = {
+    if (getType == "int") return "I"
+   
+    if (getType == "String") return "Ljava/lang/String;"
+
+    return null
+  }
+
+  override def toString: String = {
+    return `type` + " : " + name
+  }
+
 }
