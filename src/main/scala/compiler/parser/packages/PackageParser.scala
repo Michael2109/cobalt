b7cbd76e795b480c5d64c9d1f64fd9d4f624ab56
Michael <michael.jhaywoods@gmail.com>
2017-01-27 13:32:57 +0000

    Update PackageParser.scala

--- src/main/scala/compiler/parser/packages/PackageParser.scala
+++ src/main/scala/compiler/parser/packages/PackageParser.scala
@@ -7,10 +7,11 @@ import compiler.parser.Parser
 import compiler.tokenizer.Tokenizer
 
 class PackageParser extends Parser[PackageBlock] {
+
   def shouldParse(line: String): Boolean = line.matches("package [a-zA-Z][a-zA-Z0-9]*(\\.[a-zA-Z][a-zA-Z0-9]*)*")
 
   def parse(superBlock: Block, tokenizer: Tokenizer): PackageBlock = {
-    tokenizer.nextToken // import
+    tokenizer.nextToken // skip "package"
     var directory: String = tokenizer.nextToken.getToken // Get the string value of the next token.;
     var nextToken: String = tokenizer.nextToken.getToken
     while (nextToken != "") {
