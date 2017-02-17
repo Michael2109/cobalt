9e253ea94f60e88d31fdba0b26a993eb48c422af
MHaywood <michael.haywood@axa-insurance.co.uk>
2017-02-17 13:37:52 +0000

    Added missing generator files.

---
+++ src/main/scala/compiler/generators/comments/CommentGen.scala
@@ -0,0 +1,31 @@
+package compiler.block.comments
+
+import compiler.block.Block
+
+/**
+  * Represents a code comment
+  * E.g. "// This is a comment"
+  *
+  * @param superBlockInit
+  */
+class CommentGen(var superBlockInit: Block) extends Block(superBlockInit, false, false) {
+
+  // Called after file is parsed
+  override def init() {}
+
+  /* Symbol table information */
+  override def getName: String = ""
+
+  override def getValue: String = ""
+
+  override def getType: String = "comment"
+
+  /* Opening and closing byte code */
+  def getOpeningCode: String = ""
+
+  def getClosingCode: String = ""
+
+  /* toString method */
+  override def toString: String = "Comment"
+
+}
---
+++ src/main/scala/compiler/generators/primitives/DoubleGen.scala
@@ -0,0 +1,26 @@
+package compiler.block.primitives
+
+import compiler.block.Block
+
+class DoubleGen(superBlockInit: Block, declaration : Boolean, name: String,  value: String) extends Block(superBlockInit, false, true) {
+
+  def init() {}
+
+  def getName: String = name
+
+  def getValue: String = value
+
+  def getType: String = "double"
+
+  def getOpeningCode: String = {
+    return "mv.visitLdcInsn(new Double(" + value + "));\n" +
+      "mv.visitVarInsn(DSTORE, " + id + ");"
+  }
+
+  def getClosingCode: String = {
+    return ""
+  }
+
+  override def toString: String = "double: " + name + " = " + value
+
+}
