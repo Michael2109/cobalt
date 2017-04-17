238a5576172e5e2c1cd7b7e0affd3945c5401538
Michael2109 <michael.jhaywoods@gmail.com>
2017-04-17 23:45:32 +0100

    Updated Base testing class to make new tests much quicker to define.

---
+++ src/test/scala/compiler/primitives/LongTest.scala
@@ -0,0 +1,37 @@
+/*
+ * Cobalt Programming Language Compiler
+ * Copyright (C) 2017  Cobalt
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package compiler.primitives
+
+import compiler.Base
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.{BeforeAndAfter, FunSuite}
+
+@RunWith(classOf[JUnitRunner])
+class LongTest() extends FunSuite with BeforeAndAfter with Base {
+
+  test("Integer primitive test") {
+    compile()
+    val output = executeOutput()
+    assert(output(0) == "5")
+    assert(output(1) == "10000")
+
+  }
+
+}
