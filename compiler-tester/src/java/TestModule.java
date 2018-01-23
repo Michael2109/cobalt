ca100e6fcbf47eeeaab6c38e0cc938470bdf1c9a
Michael <michael.jhaywoods@gmail.com>
2018-01-23 00:22:15 +0000

    Changes cobalt code.

--- compiler-tester/src/java/TestModule.java
+++ compiler-tester/src/java/TestModule.java
@@ -1,17 +1,23 @@
-public class TestModule{
-public static void main(String[] args){
-TestModule testmodule= new TestModule();
-int x=3;
-int y=10;
-int z=15;
-int[] arrTest={x, y, z};
-System.out.println(arrTest);
-testmodule.testFunction(x);
-String z="Helloworld";}
-public void testFunction(int x){
-if(x < 50){
-System.out.println(x);
-int nextX=x + 1 * 2 - 3 / 2 + 5;
-testFunction(nextX);} else {
-int last=1000;
-System.out.println(last);}}}
+public class TestModule {
+    public static void main(String[] args) {
+        TestModule testmodule = new TestModule();
+        int x = 3;
+        int y = 10;
+        int z = 15;
+        int[] arrTest = {x, y, z};
+        System.out.println(arrTest);
+        testmodule.testFunction(x);
+        String stringTest = "Helloworld";
+    }
+
+    public void testFunction(int x) {
+        if (x < 50) {
+            System.out.println(x);
+            int nextX = x + 1 * 2 - 3 / 2 + 5;
+            testFunction(nextX);
+        } else {
+            int last = 1000;
+            System.out.println(last);
+        }
+    }
+}
