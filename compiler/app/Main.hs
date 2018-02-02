3e331d24734790a9b408525b66e88a66e4efef92
Michael <michael.jhaywoods@gmail.com>
2018-02-02 21:23:14 +0000

    Added parsing using basic indentation.

--- compiler/app/Main.hs
+++ compiler/app/Main.hs
@@ -1,19 +1,48 @@
 module Main where
 
-import Data.List
-import Parser
+--import Data.List
+--import Parser
+import IndentTest
 
+{--
 main :: IO ()
 main = do
     let inputDir = "cobalt/"
-    --let outputDir = "C:/Users/Michael/Desktop/CompilerTester/src/java/"
     let outputDir = "../compiler-tester/src/java/"
     let inputFile = inputDir ++ "TestModule.cobalt"
     let outputFile = outputDir ++ "TestModule.java"
 
+    print $ "Reading: " ++ inputFile
     s <- readFile inputFile
+
+    print "Compiling..."
     let compiledString = parseString s
 
+    print "Compiling complete."
+    print $ "Writing: " ++ outputFile
     writeFile outputFile $ compiledString
     --print $ parseString s
+
+    parsePrint s
+
+    --}
+
+main :: IO ()
+main = do
+    let inputDir = "cobalt/"
+    let outputDir = "../compiler-tester/src/java/"
+    let inputFile = inputDir ++ "IndentationTest.cobalt"
+    let outputFile = outputDir ++ "IndentationTest.java"
+
+    print $ "Reading: " ++ inputFile
+    s <- readFile inputFile
+
+    --print "Compiling..."
+    --let compiledString = parseString s
+
+    print "Compiling complete."
+    print $ "Writing: " ++ outputFile
+    --writeFile outputFile $ compiledString
+    --print $ parseString s
+
     parsePrint s
--- compiler/src/IndentTest.hs
+++ compiler/src/IndentTest.hs
@@ -10,6 +10,8 @@ import Text.Megaparsec
 import Text.Megaparsec.Char
 import qualified Text.Megaparsec.Char.Lexer as L
 
+import Block
+
 type Parser = Parsec Void String
 
 lineComment :: Parser ()
@@ -23,8 +25,25 @@ sc = L.space (void $ takeWhile1P Nothing f) lineComment empty
   where
     f x = x == ' ' || x == '\t'
 
+symbol :: String -> Parser String
+symbol = L.symbol sc
+
+rword :: String -> Parser ()
+rword w = lexeme (string w *> notFollowedBy alphaNumChar)
+
+rws :: [String] -- list of reserved words
+rws = ["module", "println", "import",  "let", "if","then","else","while","do","skip","true","false","not","and","or"]
+
 lexeme :: Parser a -> Parser a
 lexeme = L.lexeme sc
+
+identifier :: Parser String
+identifier = (lexeme . try) (p >>= check)
+  where
+    p       = (:) <$> letterChar <*> many alphaNumChar
+    check x = if x `elem` rws
+                then fail $ "keyword " ++ show x ++ " cannot be an identifier"
+                else return x
 
 
 pItemList :: Parser (String, [(String, [String])])
@@ -56,5 +75,80 @@ pLineFold = L.lineFold scn $ \sc' ->
 parser :: Parser (String, [(String, [String])])
 parser = pItemList <* eof
 
-main :: IO ()
-main = return ()
+expr :: Parser Expr
+expr = f <$> sepBy1 expr' (symbol ";")
+  where
+    -- if there's only one expr return it without using ‘Seq’
+    f l = if length l == 1 then head l else Seq l
+
+expr' :: Parser Expr
+expr' = try moduleParser
+  <|> try functionParser
+  <|> valueParser
+
+moduleParser :: Parser Expr
+moduleParser = L.nonIndented scn (L.indentBlock scn p)
+  where
+    p = do
+      rword "module"
+      name <- identifier
+      return (L.IndentSome Nothing (return . (Module name)) expr')
+
+valueParser :: Parser Expr
+valueParser = L.indentBlock scn p
+  where
+    p = do
+      valueName <- valueToken
+      symbol "="
+      return (L.IndentMany Nothing (return . (Module valueName)) expr')
+
+
+functionParser :: Parser Expr
+functionParser = L.indentBlock scn p
+  where
+    p = do
+      name <- identifier
+      symbol ":"
+      argTypes <- some argumentType
+      symbol "->"
+      rType <- IndentTest.returnType
+      nameDup <- L.lineFold scn $ \sp' ->
+        (identifier) `sepBy1` try sp' <* scn
+      args <- many argument
+      symbol "="
+      if(name == "main") then
+          return (L.IndentMany Nothing (return . (MainFunction name argTypes args rType)) expr')
+      else
+          return (L.IndentMany Nothing (return . (Function name argTypes args rType)) expr')
+
+valueToken :: Parser String
+valueToken = lexeme (takeWhile1P Nothing f) <?> "list item"
+  where
+    f x = isAlphaNum x || x == '-'
+
+valType :: Parser Expr
+valType = do
+    value <- identifier
+    return $ Type value
+
+argumentType :: Parser Expr
+argumentType = do
+    value <- identifier
+    return $ ArgumentType value
+
+returnType :: Parser Expr
+returnType = do
+    value <- identifier
+    return $ ReturnType value
+
+argument :: Parser Expr
+argument = do
+  value <- identifier
+  return $ Argument value
+
+
+exprParser :: Parser Expr
+exprParser = expr'
+
+parsePrint :: String -> IO()
+parsePrint s = parseTest' exprParser s
