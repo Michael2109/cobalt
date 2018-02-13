96b8d2989653738fa5d901fdb46cf068fead473a
Michael <michael.jhaywoods@gmail.com>
2018-02-13 14:13:44 +0000

    Added recursive compiling of directory.

--- compiler/app/Main.hs
+++ compiler/app/Main.hs
@@ -3,39 +3,25 @@ module Main where
 import Data.List
 import System.Directory
 import System.FilePath.Posix
---import Data.List
---import Parser
---import System.Directory
+
 import Parser
 
-{--
-main :: IO ()
-main = do
-    let inputDir = "cobalt/"
-    let outputDir = "../compiler-tester/src/java/"
-    let inputFile = inputDir ++ "TestModule.cobalt"
-    let outputFile = outputDir ++ "TestModule.java"
+--allFilesIn dir = filter (\filePath -> takeExtension filePath == ".cobalt")<$>(getDirectoryContents dir)
+allFilesIn dir = getDirectoryContents dir
 
-    print $ "Reading: " ++ inputFile
-    s <- readFile inputFile
 
-    print "Compiling..."
-    let compiledString = parseString s
-
-    print "Compiling complete."
-    print $ "Writing: " ++ outputFile
-    writeFile outputFile $ compiledString
-    --print $ parseString s
-
-    parsePrint s
-
-    --}
-
-allFilesIn dir = filter (\filePath -> takeExtension filePath == ".cobalt")<$>(getDirectoryContents dir)
-
-compileDir :: String -> String -> IO[()]
+compileDir :: String -> String -> IO()
 compileDir inputDir outputDir = do
-  allFilesIn inputDir >>= mapM (\inputLoc -> compile (inputDir ++ inputLoc) (outputDir ++ (dropExtension inputLoc) ++ ".java"))
+  allFilesIn inputDir >>= mapM (\inputLoc ->
+    if (takeExtension inputLoc == "")
+      then compileDir (inputDir ++ inputLoc ++ "/") (outputDir ++ inputLoc ++ "/")
+      else
+        if(takeExtension inputLoc == ".cobalt")
+        then (compile (inputDir ++ inputLoc) (outputDir ++ (dropExtension inputLoc) ++ ".java"))
+        else print ""
+    )
+  print "Compiled?"
+
 
 compile :: String -> String -> IO()
 compile inputFile outputFile = do
@@ -55,20 +41,6 @@ main = do
     let inputFile = inputDir ++ "IndentationTest.cobalt"
     let outputFile = outputDir ++ "IndentationTest.java"
 
-    print "Compiling directory"
+    print "Compiling directory..."
     compileDir inputDir outputDir
-    print "Compiled directory."
-    --compile inputDir outputDir
-
-    print $ "Reading: " ++ inputFile
-    fileData <- readFile inputFile
-
-    print "Compiling..."
-    let compiledString = parseString fileData
-
-    print "Compiling complete."
-    print $ "Writing: " ++ outputFile
-    writeFile outputFile $ compiledString
-    --print $ parseString s
-
-    parsePrint fileData
+    print "Complete."
