0826d6fedfe46ed1d8b191370b0bf3f8cf150eb9
Michael <michael.jhaywoods@gmail.com>
2018-03-08 15:45:12 +0000

    Added parent and interface to class parser test.

--- compiler/test/cobalt/parsers/expr_parsers/ClassVariableParserTest.hs
+++ compiler/test/cobalt/parsers/expr_parsers/ClassVariableParserTest.hs
@@ -15,13 +15,13 @@ import Parser
 testClassParser :: Test
 testClassParser = do
   let code = unlines [
-        "class Test extends Other implements Other"
+        "class Test extends ParentClass implements Interface"
         ]
   TestCase $ assertEqual code
-    --Class [String]String (Maybe [Expr]) (Maybe String) (Maybe String) [Expr] [Expr] [Expr] [Expr]
-    --Class packageLocs name params parent interfaces imports modifierBlocks constructorExprs bodyArray
+    --Class [String]    String (Maybe [Expr]) (Maybe String) (Maybe String) [Expr]   [Expr]         [Expr]           [Expr]
+    --Class packageLocs name   params         parent         interfaces     imports  modifierBlocks constructorExprs bodyArray
 
-    (Class [] "Test" (Nothing) (Nothing) Nothing [] [] [] [])
+    (Class [] "Test" (Nothing) (Just "ParentClass") (Just "Interface") [] [] [] [])
     (case (parse (classParser []) "" code) of
       Left  e -> Error
       Right x -> x)
