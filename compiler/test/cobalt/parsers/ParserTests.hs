73d472dc115d328f56146d3aa468819c19d79a86
Matt Lutze <mcdlutze@gmail.com>
2018-03-22 09:57:00 -0500

    add basic parentheses parser tests

--- compiler/test/cobalt/parsers/ParserTests.hs
+++ compiler/test/cobalt/parsers/ParserTests.hs
@@ -33,6 +33,7 @@ import NewClassInstanceParserTest
 import ObjectMethodCallParserTest
 import ObjectParserTest
 import ParamParserTest
+import ParenthesesParserTest
 import ReassignParserTest
 import StringLiteralParserTest
 import ThisMethodCallParserTest
@@ -115,6 +116,10 @@ parserTestList = TestList [
         testStringLiteralDoubleQuoteMultiple,
         testStringLiteralUnfinishedFail,
         testStringLiteralUnfinishedDoubleLineLeadingWhitespaceFail,
-        testStringLiteralUnfinishedDoubleLineFail
+        testStringLiteralUnfinishedDoubleLineFail,
 
+        testParenthesesVar,
+        testParenthesesNested,
+        testParenthesesNoOpenFail,
+        testParenthesesNoCloseFail
        ]
---
+++ compiler/test/cobalt/parsers/expr_parsers/ParenthesesParserTest.hs
@@ -0,0 +1,49 @@
+module ParenthesesParserTest where
+
+import Test.HUnit
+
+import Text.Megaparsec
+
+import Block
+
+import BaseParser
+import ExprParser
+
+
+testParenthesesVar :: Test
+testParenthesesVar = do
+  let code = "(x)"
+  TestCase $ assertEqual code
+    (Var "x")
+    (case (parse (parens aExpr) "" code) of
+      Left e -> Error
+      Right x -> x)
+
+testParenthesesNested :: Test
+testParenthesesNested = do
+  let code = "(((((x)))))"
+  TestCase $ assertEqual code
+    (Var "x")
+    (case (parse (parens aExpr) "" code) of
+      Left e -> Error
+      Right x -> x)
+
+testParenthesesNoOpenFail :: Test
+testParenthesesNoOpenFail = do
+  let code = "x)"
+  TestCase $ assertEqual code
+    Error
+    (case (parse (parens aExpr) "" code) of
+      Left e -> Error
+      Right x -> x)
+
+testParenthesesNoCloseFail :: Test
+testParenthesesNoCloseFail = do
+  let code = "(x"
+  TestCase $ assertEqual code
+    Error
+    (case (parse (parens aExpr) "" code) of
+      Left e -> Error
+      Right x -> x)
+
+-- TODO more complex nesting patterns
