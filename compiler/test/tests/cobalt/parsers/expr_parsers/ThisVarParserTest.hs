d89da13463a11ec05d0ba59be9c1aa3fa632fc42
Matt Lutze <mcdlutze@gmail.com>
2018-03-27 03:35:43 -0500

    add thisVarParser tests

--- compiler/test/tests/cobalt/parsers/expr_parsers/ThisVarParserTest.hs
+++ compiler/test/tests/cobalt/parsers/expr_parsers/ThisVarParserTest.hs
@@ -9,3 +9,49 @@ import Block
 import BaseParser
 import ExprParser
 import Parser
+
+testThisVarParserStartsDigitFail :: Test
+testThisVarParserStartsDigitFail = do
+  let code = "this.123"
+  TestCase $ assertEqual code
+    Error
+    (case (parse thisVarParser "" code) of
+      Left  e -> Error
+      Right x -> x)
+
+testThisVarParserContainsCapital :: Test
+testThisVarParserContainsCapital = do
+  let code = "this.ABC"
+  TestCase $ assertEqual code
+    (ThisVar $ Identifier "ABC")
+    (case (parse thisVarParser "" code) of
+      Left  e -> Error
+      Right x -> x)
+
+testThisVarParserContainsDigit :: Test
+testThisVarParserContainsDigit = do
+  let code = "this.a1b2c3"
+  TestCase $ assertEqual code
+    (ThisVar $ Identifier "a1b2c3")
+    (case (parse thisVarParser "" code) of
+      Left  e -> Error
+      Right x -> x)
+
+testThisVarParserContainsUnderscore :: Test
+testThisVarParserContainsUnderscore = do
+  let code = "this.my_var"
+  TestCase $ assertEqual code
+    (ThisVar $ Identifier "my_var")
+    (case (parse thisVarParser "" code) of
+      Left  e -> Error
+      Right x -> x)
+
+testThisVarParserNotThisFail :: Test
+testThisVarParserNotThisFail = do
+  let code = "thi.stypo"
+  TestCase $ assertEqual code
+    Error
+    (case (parse thisVarParser "" code) of
+      Left  e -> Error
+      Right x -> x)
+
