da3a15d81b2859fc24a299b0036baf6660cce1e5
FilipJanitor <filip.janitor@gmail.com>
2018-04-03 22:54:28 +0200

    Fixed style of IOUtils

--- compiler/src/cobalt/utils/IOUtils.hs
+++ compiler/src/cobalt/utils/IOUtils.hs
@@ -23,35 +23,33 @@ allFilesIn dir = getDirectoryContents dir
 
 cleanDir :: (FilePath -> Bool) -> String -> IO()
 cleanDir extensionPredicate directory = do
-  createDirectoryIfMissing True directory
-  allFilesIn directory >>= mapM (\inputLoc ->
-    if (endsWith "" inputLoc)
-      then cleanDir extensionPredicate (directory ++ inputLoc ++ "/")
-      else
-        if(extensionPredicate inputLoc)
-        then removeFile (directory ++ inputLoc)
-        else return ()
-    )
-  return ()
+    createDirectoryIfMissing True directory
+    allFilesIn directory >>= mapM (\inputLoc ->
+        if (endsWith "" inputLoc)
+        then cleanDir extensionPredicate (directory ++ inputLoc ++ "/")
+        else if(extensionPredicate inputLoc)
+             then removeFile (directory ++ inputLoc)
+             else return ()
+        )
+    return ()
 
 -- | Traverse from 'top' directory and return all the files by
 -- filtering out the 'exclude' predicate.
 traverseDir :: FilePath -> FilePath -> IO [FilePath]
 traverseDir inputDir top = do
-  ds <- getDirectoryContents $ inputDir ++ top
-  paths <- forM (ds) $ \d -> do
-    let path = top </> d
-    if endsWith "" (inputDir ++ path)
-      then traverseDir (inputDir) path
-      else return [path]
-  return (concat paths)
+    ds <- getDirectoryContents $ inputDir ++ top
+    paths <- forM (ds) $ \d -> do
+        let path = top </> d
+        if endsWith "" (inputDir ++ path)
+            then traverseDir (inputDir) path
+            else return [path]
+    return (concat paths)
 
 generateMissingDirectories :: String -> String -> IO()
 generateMissingDirectories inputDir outputDir = do
-  allFilesIn inputDir >>= mapM (\inputLoc ->
-    do
-      createDirectoryIfMissing True outputDir
-      when (endsWith "" inputLoc) $ do
-        generateMissingDirectories (inputDir ++ inputLoc ++ "/") (outputDir ++ inputLoc ++ "/")
-      )
-  return ()
+    allFilesIn inputDir >>= mapM (\inputLoc -> do
+        createDirectoryIfMissing True outputDir
+        when (endsWith "" inputLoc) $ do
+            generateMissingDirectories (inputDir ++ inputLoc ++ "/") (outputDir ++ inputLoc ++ "/")
+        )
+    return ()
