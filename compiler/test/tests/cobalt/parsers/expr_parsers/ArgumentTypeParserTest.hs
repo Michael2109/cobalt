eb07e94a3dad41cb33795ebf08a05ab51e19e17d
FilipJanitor <filip.janitor@gmail.com>
2018-04-04 17:58:43 +0200

    Fixed style of ArgumentTypeParserTest

--- compiler/test/tests/cobalt/parsers/expr_parsers/ArgumentTypeParserTest.hs
+++ compiler/test/tests/cobalt/parsers/expr_parsers/ArgumentTypeParserTest.hs
@@ -12,19 +12,19 @@ import ParserExecutor
 
 testArgumentTypeParser :: Test
 testArgumentTypeParser = do
-  let code = "ClassName"
-  TestCase $ assertEqual code
-    (ArgumentType "ClassName")
-    (case (parse (argumentTypeParser) "" code) of
-      Left  e -> Error
-      Right x -> x)
+    let code = "ClassName"
+    TestCase $ assertEqual code
+        (ArgumentType "ClassName")
+        (case (parse (argumentTypeParser) "" code) of
+             Left  e -> Error
+             Right x -> x)
 
 
 testArgumentTypeParserReservedWord :: Test
 testArgumentTypeParserReservedWord = do
-  let code = "True"
-  TestCase $ assertEqual code
-    Error
-    (case (parse (argumentTypeParser) "" code) of
-      Left  e -> Error
-      Right x -> x)
+    let code = "True"
+    TestCase $ assertEqual code
+        Error
+        (case (parse (argumentTypeParser) "" code) of
+             Left  e -> Error
+             Right x -> x)
