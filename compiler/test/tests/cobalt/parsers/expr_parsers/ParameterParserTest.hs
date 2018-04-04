0b257fd457bd54e05ae5ec621d218396e1fcd024
FilipJanitor <filip.janitor@gmail.com>
2018-04-04 22:03:43 +0200

    Fixed style of ParameterParserTest

--- compiler/test/tests/cobalt/parsers/expr_parsers/ParameterParserTest.hs
+++ compiler/test/tests/cobalt/parsers/expr_parsers/ParameterParserTest.hs
@@ -10,39 +10,38 @@ import BaseParser
 import ExprParser
 import ParserExecutor
 
-
 testParameterParser :: Test
 testParameterParser = do
-  let code = "x: Int"
-  TestCase $ assertEqual code
-    (Parameter (Identifier "Int") (Identifier "x"))
-    (case (parse (parameterParser) "" code) of
-      Left e -> Error
-      Right x -> x)
+    let code = "x: Int"
+    TestCase $ assertEqual code
+        (Parameter (Identifier "Int") (Identifier "x"))
+        (case (parse (parameterParser) "" code) of
+             Left e -> Error
+             Right x -> x)
 
 testParameterParserMissingVar :: Test
 testParameterParserMissingVar = do
-  let code = ": Int"
-  TestCase $ assertEqual code
-    Error
-    (case (parse (parameterParser) "" code) of
-      Left e -> Error
-      Right x -> x)
+    let code = ": Int"
+    TestCase $ assertEqual code
+        Error
+        (case (parse (parameterParser) "" code) of
+             Left e -> Error
+             Right x -> x)
 
 testParameterParserMissingType :: Test
 testParameterParserMissingType = do
-  let code = "x: "
-  TestCase $ assertEqual code
-    Error
-    (case (parse (parameterParser) "" code) of
-      Left e -> Error
-      Right x -> x)
+    let code = "x: "
+    TestCase $ assertEqual code
+        Error
+        (case (parse (parameterParser) "" code) of
+             Left e -> Error
+             Right x -> x)
 
 testParameterParserMissingColon :: Test
 testParameterParserMissingColon = do
-  let code = "x Int"
-  TestCase $ assertEqual code
-    Error
-    (case (parse (parameterParser) "" code) of
-      Left e -> Error
-      Right x -> x)
+    let code = "x Int"
+    TestCase $ assertEqual code
+        Error
+        (case (parse (parameterParser) "" code) of
+             Left e -> Error
+             Right x -> x)
