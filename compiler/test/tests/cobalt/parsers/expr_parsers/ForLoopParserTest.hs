f2e117372e22bc896b031c613f70734ca8d2798c
FilipJanitor <filip.janitor@gmail.com>
2018-04-04 22:14:33 +0200

    Fixed style of ForLoopParserTest

--- compiler/test/tests/cobalt/parsers/expr_parsers/ForLoopParserTest.hs
+++ compiler/test/tests/cobalt/parsers/expr_parsers/ForLoopParserTest.hs
@@ -12,12 +12,11 @@ import ParserExecutor
 
 testForLoopParser :: Test
 testForLoopParser = do
-  let code = unlines [
-        "for(i <- 0 to 10)",
-        "  println(i)"
-        ]
-  TestCase $ assertEqual code
-    (For "i" (ArithExpr (IntConst 0)) (ArithExpr (IntConst 10)) [Print (Argument (BooleanExpr (Identifier "i")))])
-    (case (parse (forLoopParser) "" code) of
-      Left  e -> Error
-      Right x -> x)
+    let code = unlines [ "for(i <- 0 to 10)"
+                       , "  println(i)"
+                       ]
+    TestCase $ assertEqual code
+        (For "i" (ArithExpr (IntConst 0)) (ArithExpr (IntConst 10)) [Print (Argument (BooleanExpr (Identifier "i")))])
+        (case (parse (forLoopParser) "" code) of
+             Left  e -> Error
+             Right x -> x)
