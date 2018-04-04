37f79cddd6e8c9168647acb470659e9f7d7541d4
FilipJanitor <filip.janitor@gmail.com>
2018-04-04 22:04:30 +0200

    Fixed style of ParenthesesParserTest

--- compiler/test/tests/cobalt/parsers/expr_parsers/ParenthesesParserTest.hs
+++ compiler/test/tests/cobalt/parsers/expr_parsers/ParenthesesParserTest.hs
@@ -9,39 +9,38 @@ import Block
 import BaseParser
 import ExprParser
 
-
 testParenthesesParserVar :: Test
 testParenthesesParserVar = do
-  let code = "(x)"
-  TestCase $ assertEqual code
-    (Identifier "x")
-    (case (parse (parens aExpr) "" code) of
-      Left e -> Error
-      Right x -> x)
+    let code = "(x)"
+    TestCase $ assertEqual code
+        (Identifier "x")
+        (case (parse (parens aExpr) "" code) of
+             Left e -> Error
+             Right x -> x)
 
 testParenthesesParserNested :: Test
 testParenthesesParserNested = do
-  let code = "(((((x)))))"
-  TestCase $ assertEqual code
-    (Identifier "x")
-    (case (parse (parens aExpr) "" code) of
-      Left e -> Error
-      Right x -> x)
+    let code = "(((((x)))))"
+    TestCase $ assertEqual code
+        (Identifier "x")
+        (case (parse (parens aExpr) "" code) of
+             Left e -> Error
+             Right x -> x)
 
 testParenthesesParserNoOpenFail :: Test
 testParenthesesParserNoOpenFail = do
-  let code = "x)"
-  TestCase $ assertEqual code
-    Error
-    (case (parse (parens aExpr) "" code) of
-      Left e -> Error
-      Right x -> x)
+    let code = "x)"
+    TestCase $ assertEqual code
+        Error
+        (case (parse (parens aExpr) "" code) of
+             Left e -> Error
+             Right x -> x)
 
 testParenthesesParserNoCloseFail :: Test
 testParenthesesParserNoCloseFail = do
-  let code = "(x"
-  TestCase $ assertEqual code
-    Error
-    (case (parse (parens aExpr) "" code) of
-      Left e -> Error
-      Right x -> x)
+    let code = "(x"
+    TestCase $ assertEqual code
+        Error
+        (case (parse (parens aExpr) "" code) of
+             Left e -> Error
+             Right x -> x)
