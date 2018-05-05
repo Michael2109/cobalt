62e54414a7c0bbda31eda8c0ac011609c1534f06
Michael <michael.jhaywoods@gmail.com>
2018-05-05 16:58:58 +0100

    Added model parser tests.

--- compiler/test/tests/cobalt/Parser/ModelParserTest.hs
+++ compiler/test/tests/cobalt/Parser/ModelParserTest.hs
@@ -12,10 +12,21 @@ testModelParser = do
     let code = "class Test"
     let test = testParseSuccess code (Model (Name "Test") ClassModel [] [] Nothing [] [] (BlockStmt [])) modelParser
 
+    let codeExtends = "class Test extends Parent"
+    let testExtends = testParseSuccess codeExtends (Model {modelName = Name "Test", modelType = ClassModel, modelModifiers = [], modelFields = [], modelParent = Just (TypeRef (RefLocal (Name "Parent"))), modelParentArguments = [], modelInterfaces = [], modelBody = BlockStmt []}) modelParser
+
+    let codeImplements = "class Test implements Interface"
+    let testImplements = testParseSuccess codeImplements (Model {modelName = Name "Test", modelType = ClassModel, modelModifiers = [], modelFields = [], modelParent = Nothing, modelParentArguments = [], modelInterfaces = [TypeRef (RefLocal (Name "Interface"))], modelBody = BlockStmt []}) modelParser
+
+    let codeExtendsImplements = "class Test extends Parent implements "
+    let testExtendsImplements = testParseSuccess code (Model (Name "Test") ClassModel [] [] Nothing [] [] (BlockStmt [])) modelParser
+
     let codeInner = unlines [ "class OuterClass"
                             , "    class InnerClass"]
     let testInner = testParseSuccess codeInner (Model {modelName = Name "OuterClass", modelType = ClassModel, modelModifiers = [], modelFields = [], modelParent = Nothing, modelParentArguments = [], modelInterfaces = [], modelBody = BlockStmt [ModelDef (Model {modelName = Name "InnerClass", modelType = ClassModel,  modelModifiers = [], modelFields = [], modelParent = Nothing, modelParentArguments = [], modelInterfaces = [], modelBody = (BlockStmt [])})]}) modelParser
 
     TestList [ test
+             , testExtends
+             , testImplements
              , testInner
              ]
