d9f955b862e53557ed5ed72fd0b98f6123365fbe
FilipJanitor <filip.janitor@gmail.com>
2018-05-11 12:56:18 +0200

    Added check to make sure whole input is parsed when testing

--- compiler/test/tests/TestUtil/ParserTestUtil.hs
+++ compiler/test/tests/TestUtil/ParserTestUtil.hs
@@ -9,7 +9,7 @@ import Parser.Parser
 
 testParseSuccess code result parser = TestCase $ assertEqual code
     result
-    (case (parse parser "" code) of
+    (case (parse (parser <* eof ) "" code) of
         Left  e -> error $ "(" ++ code ++ ") - " ++ show e
         Right x -> x)
 
