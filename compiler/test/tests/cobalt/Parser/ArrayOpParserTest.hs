03279ba30c6b353d12ad4612eed8a994770f36be
Michael <michael.jhaywoods@gmail.com>
2018-05-14 22:28:24 +0100

    Added extra tests.

--- compiler/test/tests/cobalt/Parser/ArrayOpParserTest.hs
+++ compiler/test/tests/cobalt/Parser/ArrayOpParserTest.hs
@@ -17,7 +17,15 @@ testArrayOpParser = do
     let codeMultipleStringLiteral = "\"String Literal 1\" ++ x"
     let testMultipleStringLiteral = testParseSuccess codeMultipleStringLiteral (Array ArrayAppend (StringLiteral "String Literal 1")  (Identifier (Name "x"))) expressionParser'
 
+    let codeMultipleMixed = "\"String Literal 1\" ++ ClassName.varName ++ methodCall() ++ varName"
+    let testMultipleMixed = testParseSuccess codeMultipleMixed (Array ArrayAppend (Array ArrayAppend (Array ArrayAppend (StringLiteral "String Literal 1") (BlockExpr [Identifier (Name "ClassName"),Identifier (Name "varName")])) (MethodCall (Name "methodCall") (BlockExpr []))) (Identifier (Name "varName"))) expressionParser'
+
+    let codeParenthesis = "(\"String Literal 1\" ++ ClassName.varName) ++ methodCall() ++ varName"
+    let testParenthesis = testParseSuccess codeParenthesis (Array ArrayAppend (Array ArrayAppend (Array ArrayAppend (StringLiteral "String Literal 1") (BlockExpr [Identifier (Name "ClassName"),Identifier (Name "varName")])) (MethodCall (Name "methodCall") (BlockExpr []))) (Identifier (Name "varName"))) expressionParser'
+
     TestList [ testArrayOp
              , testStringLiteral
              , testMultipleStringLiteral
+             , testMultipleMixed
+             , testParenthesis
              ]
