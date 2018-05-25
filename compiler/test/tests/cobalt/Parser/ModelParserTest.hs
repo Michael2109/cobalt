7b1c10b0dc095cc7be436df0bcf06876493cb775
Matt Lutze <mcdlutze@gmail.com>
2018-05-25 14:25:28 -0500

    replace pure method model parser with pure model parser

--- compiler/test/tests/cobalt/Parser/ModelParserTest.hs
+++ compiler/test/tests/cobalt/Parser/ModelParserTest.hs
@@ -25,6 +25,9 @@ testModelParser = do
                             ]
     let testInner = testParseSuccess codeInner (Model {modelName = Name "OuterClass", modelType = ClassModel, modelModifiers = [], modelFields = [], modelParent = Nothing, modelParentArguments = [], modelInterfaces = [], modelBody = BlockStmt [ModelDef (Model {modelName = Name "InnerClass", modelType = ClassModel,  modelModifiers = [], modelFields = [], modelParent = Nothing, modelParentArguments = [], modelInterfaces = [], modelBody = (BlockStmt [])})]}) modelParser
 
+    let codePureModel = "pure class Test"
+    let testPureModel = testParseSuccess codePureModel (Model {modelName = Name "Test", modelType = ClassModel, modelModifiers = [Pure], modelFields = [], modelParent = Nothing, modelParentArguments = [], modelInterfaces = [], modelBody = BlockStmt []}) modelParser
+
     let codeMethods = unlines [ "class OuterClass"
                               , "    let x() = 10"
                               , "    let y() = do"
@@ -36,22 +39,12 @@ testModelParser = do
                               ]
     let testMethods = testParseSuccess codeMethods (Model {modelName = Name "OuterClass", modelType = ClassModel, modelModifiers = [], modelFields = [], modelParent = Nothing, modelParentArguments = [], modelInterfaces = [], modelBody = BlockStmt [MethodDef (Method {methodName = Name "x", methodAnns = [], methodFields = [], methodModifiers = [], methodReturnType = Nothing, methodBody = Inline (IntConst 10)}),MethodDef (Method {methodName = Name "y", methodAnns = [], methodFields = [], methodModifiers = [], methodReturnType = Nothing, methodBody = DoBlock (BlockStmt [MethodDef (Method {methodName = Name "x", methodAnns = [], methodFields = [], methodModifiers = [], methodReturnType = Nothing, methodBody = DoBlock (BlockStmt [If (BoolConst True) (BlockStmt [ExprAsStmt (IntConst 10)]) (Just (BlockStmt [ExprAsStmt (IntConst 20)]))])})])})]}) modelParser
 
-    let codePureMethods = unlines [ "class OuterClass"
-                                  , "    pure let x() = 10"
-                                  , "    pure let y() = do"
-                                  , "        pure let x() = do"
-                                  , "            if True then"
-                                  , "                10"
-                                  , "            else"
-                                  , "                20"
-                                  ]
-    let testPureMethods = testParseSuccess codePureMethods (Model {modelName = Name "OuterClass", modelType = ClassModel, modelModifiers = [], modelFields = [], modelParent = Nothing, modelParentArguments = [], modelInterfaces = [], modelBody = BlockStmt [MethodDef (Method {methodName = Name "x", methodAnns = [], methodFields = [], methodModifiers = [Pure], methodReturnType = Nothing, methodBody = Inline (IntConst 10)}),MethodDef (Method {methodName = Name "y", methodAnns = [], methodFields = [], methodModifiers = [Pure], methodReturnType = Nothing, methodBody = DoBlock (BlockStmt [MethodDef (Method {methodName = Name "x", methodAnns = [], methodFields = [], methodModifiers = [Pure], methodReturnType = Nothing, methodBody = DoBlock (BlockStmt [If (BoolConst True) (BlockStmt [ExprAsStmt (IntConst 10)]) (Just (BlockStmt [ExprAsStmt (IntConst 20)]))])})])})]}) modelParser
 
     TestList [ testModel
              , testExtends
              , testImplements
              , testExtendsImplements
              , testInner
+             , testPureModel
              , testMethods
-             , testPureMethods
              ]
