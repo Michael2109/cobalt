488f48ffeb812c66b7fb496be8e58eccde604d72
Michael <michael.jhaywoods@gmail.com>
2018-06-29 17:35:02 +0100

    Added a string literal code generation test.

---
+++ src/test/scala/cobalt/integration/StringLiteralCompileTest.scala
@@ -0,0 +1,16 @@
+package cobalt.integration
+
+import cobalt.utils.CompilerUtil
+import org.scalatest.Matchers
+
+class StringLiteralCompileTest extends IntegrationSetup with Matchers {
+
+  describe("String Literal compile test") {
+    it("Should compile string literals") {
+
+      val output: Array[String] = CompilerUtil.executeJava("cobalt_generated", "integration/StringLiteralTest")
+      output(0) shouldBe "This is some text"
+    }
+  }
+
+}
