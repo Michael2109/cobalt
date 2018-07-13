1bcc6ebd20164e32f042d3d3e268f3c20480c0fe
Michael <michael.jhaywoods@gmail.com>
2018-07-13 15:47:50 +0100

    Added methods for searching jar files for classes.

---
+++ src/main/scala/cobalt/jar_loader/JarUtility.scala
@@ -0,0 +1,50 @@
+package cobalt.jar_loader
+
+import java.io.{File, FileFilter}
+import java.nio.file.{Path, Paths}
+import java.util.jar.JarFile
+import java.util.zip.ZipEntry
+
+import scala.collection.mutable.ListBuffer
+
+object JarUtility {
+
+
+  val JavaHome = Paths.get(System.getenv().get("JAVA_HOME"))
+
+  def main(args: Array[String]): Unit = {
+
+    println(findJar(JavaHome.toFile, "java/lang/Double.class"))
+
+  }
+
+  def findJar(start: File, className: String): Path ={
+
+    val filter = new FileFilter() {
+      def accept(pathname: File): Boolean = {
+        return pathname.getName().endsWith(".jar") || pathname.isDirectory()
+      }
+    }
+    for (f: File <- start.listFiles(filter)) {
+      if (f.isDirectory()) {
+        return findJar(f, className)
+      } else {
+        return searchJar(f, className)
+      }
+    }
+  }
+
+  private def searchJar(f: File, className: String): Path ={
+
+    println("Searching: " + f.getPath())
+    val jar = new JarFile(f)
+    var e: ZipEntry = jar.getEntry(className)
+    if (e == null) {
+      e = jar.getJarEntry(className)
+      f.toPath
+    } else {
+      f.toPath
+    }
+  }
+
+}
